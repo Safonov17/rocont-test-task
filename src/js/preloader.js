a0731193b02e6623b14725b366c75021b0ada469
Safonov Ilia <safonovil17@yandex.ru>
2024-08-15 17:46:32 +0300

    fix animation

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,8 +2,8 @@ export const preloader = () => {
 	window.addEventListener('load', function () {
 		var preloader = document.getElementById('preloader')
 		preloader.style.display = 'none'
-		// AOS.init({
-		// 	disable: 'mobile'
-		// })
+		AOS.init({
+			disable: 'mobile'
+		})
 	})
 }
