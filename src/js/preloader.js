f230297fddc17ae430f2452217b04a1309f47009
Safonov Ilia <safonovil17@yandex.ru>
2024-08-15 16:26:56 +0300

    add preloader

---
+++ src/js/preloader.js
@@ -0,0 +1,7 @@
+export const preloader = () => {
+	window.addEventListener('load', function () {
+		var preloader = document.getElementById('preloader')
+		preloader.style.display = 'none'
+		AOS.init()
+	})
+}
--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,10 @@ import { showMenu } from './burger'
 import { formValidation } from './form-validation'
 import { gallerySlider } from './gallery-slider'
 import { showMap } from './map'
+import { preloader } from './preloader'
 
+preloader()
 showMenu()
 gallerySlider()
 showMap()
 formValidation()
-AOS.init()
